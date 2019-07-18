be6229890950ad1ab29975ae8c5c2e556af2ff32
Anurag Bhattacharya <abhatt66@my.centennialcollege.ca>
2019-07-18 00:03:56 -0400

    Week 10 Example Finished

--- COMP123-S2019-Lesson10C/Program.cs
+++ COMP123-S2019-Lesson10C/Program.cs
@@ -8,6 +8,8 @@ namespace COMP123_S2019_Lesson10C
 {
     static class Program
     {
+        public static Dictionary<FormName, Form> Forms;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +18,13 @@ namespace COMP123_S2019_Lesson10C
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Forms = new Dictionary<FormName, Form>();
+            Forms.Add(FormName.START_FORM, new StartForm());
+            Forms.Add(FormName.MAIN_FORM, new MainForm());
+            Forms.Add(FormName.END_FORM, new EndForm());
+
+            Application.Run(Forms[FormName.START_FORM]);
         }
     }
 }
